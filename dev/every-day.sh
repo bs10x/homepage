c8c475788c170bed800e6f5183c32b66fe91c5d1
bs10x <bfeitknecht@student.ethz.ch>
2023-10-27 10:35:46 +0200

    reset to commit 0b12190 to delete unrelated history

--- dev/every-day.sh
+++ dev/every-day.sh
@@ -3,10 +3,17 @@
 # Navigate to the directory containing the HTML file
 cd ~/homepage/dev
 
-# Get the current date in the desired format
-current_date=$(date +"%a. %d.%m.%Y")
+# Get the current date and time
+now=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Update the HTML file with the current date
-sed -i "s/<p>Last updated: .*<\/p>/<p>Last updated: $current_date<\/p>/" every-day.html
+# Update the every-day.html file with the current date and time
+sed -i "s/<span id=\"modified\">.*<\/span>/<span id=\"modified\">$now<\/span>/" every-day.html
 
+# Add all changes to the staging area
+git add ./
 
+# Commit the changes with a commit message
+git commit -m "daily commit"
+
+# Push the changes to the upstream remote origin master branch
+git push -u origin master
